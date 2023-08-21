cd904f5742707419e289200450edcf005502e824
Eduardo Fleury <eduardopinafleury@gmail.com>
2023-08-21 15:34:36 -0300

    Repositories parte 1

--- src/pages/RepositoriesPages/index.jsx
+++ src/pages/RepositoriesPages/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Profile from './Profile'
 import Filter from './Filter'
+import Repositories from '../Repositories'
 
 import { Container, SideBar, Main } from './styles'
 
@@ -13,7 +14,7 @@ function RepositoriesPages() {
        <Filter/>
       </SideBar>
       <Main>
-        Main
+        <Repositories/>
       </Main>
     </Container>
   )
