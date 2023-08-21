1d2ed2f7b614e398b6976a5b9b0cf571c0281116
Eduardo Fleury <eduardopinafleury@gmail.com>
2023-08-21 09:10:50 -0300

    Dados do usuário parte 2

--- src/pages/RepositoriesPages/Profile/index.jsx
+++ src/pages/RepositoriesPages/Profile/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import { Container, Header , Avatar, Login, Name} from './styles'
+import {MdGroup, MdLocationCity, MdWork, MdLink} from 'react-icons/md'
+
+import { Container, Header , Avatar, Login, Name, Inner, Data} from './styles'
 
 
 function Profile() {
@@ -11,6 +13,24 @@ function Profile() {
       <Login>EduFleury</Login>
       <Name>Eduardo Fleury</Name>
     </Header>
+    <Inner>
+      <Data>
+        <MdGroup size={20}/>
+        30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+      </Data>
+      <Data>
+        <MdWork size={20}/>
+        Vatec
+      </Data>
+      <Data>
+        <MdLocationCity size={20}/>
+        Goiânia - GO
+      </Data>
+      <Data>
+        <MdLink size={20}/>&nbsp;
+        <a href='https://www.linkedin.com/in/eduardo-pina-fleury-fortuna-51a57021b/'>Linkedin</a>
+      </Data>
+    </Inner>
    </Container>
   )
 }
